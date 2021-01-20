5f5cb4f07c632e0f79aa498a4c768445de53e584
Moja One <joalex09@gmail.com>
2021-01-20 09:01:56 -0500

    app.js reset

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,5 @@
-// Setting function on dropdownmenu to print new graph based on option selected
-d3.selectAll("#selDataset").on("change", selection)
 
-// create function to make dropdown value
-    // Select the dropdownmenu with d3
-d3.json("data/samples.json").then(function(dataOtu){
-    
+//Read samples.json
+function getPlots(id) {
+    d3.json("samples.json").then (sampledata =>{
+        console.log(sampledata)
