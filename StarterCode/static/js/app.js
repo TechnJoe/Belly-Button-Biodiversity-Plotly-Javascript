befcca409ad743b0622f42be804130bd74a3036a
Moja One <joalex09@gmail.com>
2021-01-20 09:45:44 -0500

    OTU and label ploting

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,3 +3,23 @@
 function getPlots(id) {
     d3.json("samples.json").then (sampledata =>{
         console.log(sampledata)
+        var ids = sampledata.samples[0].otu_ids;
+        console.log(ids)      
+        var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
+        console.log(sampleValues)
+        var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+        console.log (labels)
+    
+        // get top 10 otu ids for the plot OTU and reverse it. 
+        var OTU_top = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+    
+        // get the otu id's to the desired form for the plot
+        var OTU_id = OTU_top.map(d => "OTU " + d);
+        console.log(`OTU IDS: ${OTU_id}`)
+     
+        // get the top 10 labels for the plot
+        var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+        console.log(`OTU_labels: ${labels}`)
+    }
+
+}
