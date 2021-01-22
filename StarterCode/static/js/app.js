d7b8ce74333a588ee09b44fc5bc9b65c60d9c121
Moja One <joalex09@gmail.com>
2021-01-22 10:30:15 -0500

    demographic update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -47,26 +47,45 @@ function getPlots(id) {
             // create the bar plot
         Plotly.newPlot("bar", data, layout);
             // The bubble chart
-            var trace1 = {
-                x: sampledata.samples[0].otu_ids,
-                y: sampledata.samples[0].sample_values,
-                mode: "markers",
-                marker: {
-                    size: sampledata.samples[0].sample_values,
-                    color: sampledata.samples[0].otu_ids
-                },
-                text:  sampledata.samples[0].otu_labels
+            // var trace1 = {
+            //     x: sampledata.samples[0].otu_ids,
+            //     y: sampledata.samples[0].sample_values,
+            //     mode: "markers",
+            //     marker: {
+            //         size: sampledata.samples[0].sample_values,
+            //         color: sampledata.samples[0].otu_ids
+            //     },
+            //     text:  sampledata.samples[0].otu_labels
     
-            };
+            // };
+    });
     
-            
-            
-        });
-    
-        }
-
-        //create the the funtion to get the necessary metadata
+    }  
+           
+         //create the the funtion to get the necessary metadata
+         // read the json file to get data
         function getDemoInfo(id) {
+            d3.json("samples.json").then((data)=> {
+        
+                // get the metadata info for the demographic panel
+                var metadata = data.metadata;
+
+                console.log(metadata)
+    
+                // filter meta data info by id
+                 var result = metadata.filter(meta => meta.id.toString() === id)[0];
+                // select demographic panel to put data
+                 var demographicInfo = d3.select("#sample-metadata");
+                  
+               // empty the demographic info panel each time before getting new id info
+                 demographicInfo.html("");
+          
+               // grab the necessary demographic data data for the id and append the info to the panel
+                  Object.entries(result).forEach((key) => {   
+                      demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
+                  });
+              });
+        
 
         }
 
@@ -86,9 +105,42 @@ function getPlots(id) {
         function optionChanged(id){
             getPlots(id);
         } 
-
+    
         populateDropdown();
+    
+    
+         // The bubble chart
+        var trace1 = {
+            x: sampledata.samples[0].otu_ids,
+            y: sampledata.samples[0].sample_values,
+            mode: "markers",
+            marker: {
+                size: sampledata.samples[0].sample_values,
+                 color: sampledata.samples[0].otu_ids
+            },
+            text:  sampledata.samples[0].otu_labels
+
+        };
 
         // set the layout for the bubble plot
 
+        var layout_2 = {
+            xaxis:{title: "OTU ID"},
+            height: 600,
+            width: 1000
+        };
+
+        // creating data variable 
+        var data1 = [trace1];
+
+        // create the bubble plot
+        Plotly.newPlot("bubble", data1, layout_2); 
+    
+    
+ 
+ // create the function for the change event
+ function optionChanged(id) {
+    getPlots(id);
+    getDemoInfo(id);
+}
 
